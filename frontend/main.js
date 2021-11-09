e5c12a5bdcb4c0e5d2da467f644d968785ca5518
Mateus Victor <mateus_victors@outlook.com>
2021-11-09 17:55:08 -0300

    add comments on main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,15 @@
-// var percentage = document.getElementsByClassName('percentage')[0]
-// percentage.style['transform'] = 'rotate(170deg)'
+// main.js
+// Mateus Victor - 2021
 
-const updateTime = 5000
+const updateTime = 8000 // Intervalo a cada atualização do dashboard em ms
+const prototypeId = 1 // ID do protótipo HidroINO
+
 let counter = 1
 
-let main = setInterval(requestAPI, 5000)
+let main = setInterval(requestAPI, updateTime) // A cada `updateTime` ms a função `requestAPI` é chamada. 	
 function requestAPI(){
-	let url = 'http://localhost:8000/record-last/1/';
+	// Realiza uma requisição do tipo GET e repassa os dados para a função `update`
+	let url = 'http://localhost:8000/status/' + String(prototypeId) + '/';
 	let response;
 	fetch(url)
 	    .then(res => res.json())
@@ -16,9 +19,27 @@ function requestAPI(){
 
 
 function update(response){
+	// Seleciona e atualiza os valores presentes nos elementos `gauge`
+	// Exemplo de `response`:
+	// {
+	// 	prototype_id: 1,
+	// 	ph: 6.1,
+	// 	temperature: 30.1,
+	// 	humidity: 71,
+	// 	datetime_creation: "11-09-2021 17:30:03"
+	// }
+
+
 	let phGauge = document.getElementById('ph').children;
 	let tempGauge = document.getElementById('temp').children;
 	let humidityGauge = document.getElementById('humidity').children;
+	// Cada Gauge possui 3 "childrens": {div-percentage, div-mask, div-value}
+	//
+	// * div-percentage ou Gauge[0] é o elemento que abriga a barra de progresso:
+	// 		- A partir da mudança do style desse elemento que ocorre a animação
+	//
+	// * div-value ou Gauge[2] é o elemento que possui a valor do medidor
+
 
 	tempPercentage = tempGauge[0], tempValue = tempGauge[2];
 	phPercentage = phGauge[0], phValue = phGauge[2];
@@ -28,6 +49,7 @@ function update(response){
 	phValue.innerHTML = response['ph'];
 	humidityValue.innerHTML = response['humidity']+ "%";
 
+
 	moveGauge(tempPercentage, response['temperature'] / 40)
 	moveGauge(phPercentage, response['ph'] / 14)
 	moveGauge(humidityPercentage, response['humidity'] / 100)
@@ -36,7 +58,12 @@ function update(response){
 
 
 function moveGauge(actualPercent, toMove){
-	let actualDeg = Math.floor(strToFloat(actualPercent.style['transform']));
+	// Realiza a animação de crescimento/decrescimento da barra de progresso do medidor
+	// actualPercent: porcentagem atual de preenchimento da barra da progresso
+	// toMove: porcentagem desejada de preenchimento da barra de progresso
+
+
+	let actualDeg = Math.floor(strToFloat(actualPercent.style['transform'])); // Angulação, em graus, da barra de progresso
 	let newDeg = Math.floor(toMove * 180);
 
 	console.log(counter, actualDeg, newDeg)
@@ -44,8 +71,9 @@ function moveGauge(actualPercent, toMove){
 	let i = 0	
 	if (i == 0){
 		i = 1
-		let change = setInterval(frame, 5);
+		let change = setInterval(frame, 5); // Chama a função `frame` uma vez a cada 5 ms
 		function frame(){
+			// Incrementa/decrementa 1 grau a cada 5ms da rotação da barra de progresso do Gauge
 			if (actualDeg == newDeg){
 				i = 0;
 				clearInterval(change);
